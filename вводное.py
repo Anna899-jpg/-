3f991efe23bc0a28ef26b9d3b87682ca107dcc3e
Anna899-jpg <an.platonova2000@gmail.com>
2024-07-23 12:04:11 +0500

    Add files via upload

---
+++ вводное.py
@@ -0,0 +1,21 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print (9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+a = 1234
+b = 5678
+c = 1234 // 10
+d = c % 100
+e = 5678 // 10
+q = e % 100
+w = d + q
+print(w)
+# 4th program
+a = 13.42
+b = 42.13
+c = (a * 100) % 100
+d = (b * 100) % 100
+f = int(a)
+e = int(b)
+print (int(c)) == e or (int(d)) == f
