7bb19aa9dffc8e600627b73fef6ceadad5d1afa3
Anna899-jpg <an.platonova2000@gmail.com>
2024-07-23 11:33:57 +0500

    Add files via upload

---
+++ Цикл for. Элементы списка. Полезные функции в цикле11.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+for i in numbers:
+    print(i)
+primes = []
+not_primes = []
+for j in numbers:
+    if numbers // 2:
+        print(j)
