7adbc1584f9ad8edad5ef7b0bbbfa09f8fa9d6f8
Anna899-jpg <an.platonova2000@gmail.com>
2024-08-05 14:59:04 +0500

    Add files via upload

---
+++ Распаковка позиционных параметров1.py
@@ -0,0 +1,17 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+list = [2, "hi", True]
+print_params(b = 25)
+print_params(c = [1,2,3])
+# 2
+values_list = (5, 'cat', True)
+print(values_list)
+
+values_dict = {'a': 5, 'b': 'cat', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+#3
+values_list_2 = (3, 'cat')
+print_params(*values_list_2, 42)
+
